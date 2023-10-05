703e5a6a61e83a58eae527654468e06ffa13ade2
Dinesh Siriwardhane <dinesh.siriwardhane@consultek.com.au>
2023-10-05 16:00:38 +1100

    Add sample app file and corresponding Jenkinsfile.

---
+++ sample-app/app.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import sys
+
+print("Congratulations! You have successfully cloned AWS CodeCommit repository using IAM role to Kubernetes service account bindings!")
+sys.exit()
